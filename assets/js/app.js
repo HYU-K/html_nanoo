7be2c64aca4f1eace97fdf65bdd5103a36a3e603
HYU-K <85468352+HYU-K@users.noreply.github.com>
2021-08-10 09:01:11 +0700

    huy add

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,14 @@ let menu_list = {
     1: {
         "link": "02_gioithieu",
         "title": "GIỚI THIỆU",
+        "sub_menu": {
+            "link": "",
+            "title": "",
+        },
+
     },
+
+
     2: {
         "link": "",
         "title": "SẢN PHẨM",
@@ -36,6 +43,7 @@ let renderHeader = () => {
     var logo = '<div class="container"><div class="row"><div class="header"><div class="logo"><a href="" title=""><img src="./assets/img/logo1.png"></a></div>';
     var menu = '<div class="menu-top"><div class="menu-wrapper"><div class="hamburger"><span></span><span></span><span></span></div></div>';
     var list = '<ul class="menu-list accordion"><li class="logo-menu"><a href="" title=""><img src="./assets/img/logo1.png" alt=""></a></li>';
+    var sub_menu = '<div class="sub-menu-1"><div class="a-lv-1"><a class="title5" href="" title="">Sơn nội thất</a></div><div class="a-lv-1"><a class="title5" href="" title="">Chống thấm</a></div><div class="a-lv-1 fa-angle"><a class="title5" href="" title="">Sơn ngoại thất</a><div class="menu2"><div><a class="title5" href="" title="">Matit</a></div><div><a class="title5" href="" title="">Sơn lót</a></div><div><a class="title5" href="" title="">Sơn phủ</a></div></div></div></div><span class="fa fa-angle-down rotate-lv-1"></span>'
     var searchbar = '<div class="searchbar"><span class="fa-search"><img src="./assets/img/seach.png" alt=""></span><div class="togglesearch"><input type="text" placeholder="" /><input type="button" value="Search" /></div></div>';
     $.each(menu_list, function (index, value) {
         list += '<li><a class="menu1 title5" href="' + value.link + '.html" title="">' + value.title + '</a></li>'
